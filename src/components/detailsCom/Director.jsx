ec8a63d7caaa8f15edd56fa03244a24039516212
25LP0882 25LP0882 <25LP0882@admins-iMac.local>
2025-05-26 16:16:11 +0800

    mm

--- src/components/detailsCom/Director.jsx
+++ src/components/detailsCom/Director.jsx
@@ -4,24 +4,37 @@ import Link from "next/link";
 import { getDirector } from "@/lib/api/get-director";
 
 export const Director = ({ id }) => {
-  const [director, setDirector] = useState([]);
+  const [directors, setDirector] = useState([]);
   useEffect(() => {
     if (!id) return;
-    const director = async () => {
+    const fetchdDirector = async () => {
+      try {
       const data = await getDirector(id);
-      console.log(data);
-      setDirector(data);
-    };
+           console.log("Raw director data:", data);
 
-    director();
+        const isDirector = data?.crew?.filter((person) => person.job === "Director")
+     
+        setDirector(isDirector);
+     
+    }catch (error) {
+        console.error("Failed to fetch directors:", error);
+      }
+  }
+
+  fetchdDirector();
   }, [id]);
 
   return (
-    <div className="flex-col  flex gap-y-[33px]  divide-y">
-      <div className="flex px-[20px] gap-13">
-        <p className="text-[16px] font-bold w-[64px]">Director</p>
-        <p className="text-[16px]">Jon M. Chu</p>
-      </div>
+    
+  <div className="flex-col flex gap-y-[33px] divide-y">
+      {directors.map((person) => (
+        <div key={person.id} className="flex px-[20px] gap-13">Director
+          <p className="text-[16px] font-bold w-[64px] capitalize">
+            {person.name}
+          </p>
+          <p className="text-[16px]">{id?.name}</p>
+        </div>
+      ))}
       <div className="flex px-[20px] gap-13">
         <p className="text-[16px] font-bold w-[64px]">Writers</p>
         <p className="text-[16px]">
